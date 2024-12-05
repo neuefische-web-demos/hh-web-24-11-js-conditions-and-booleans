29b733767b698788e5b8d3e4fc742faa83c59d3a
Roland-Hufnagel <roland.hufnagel@gmx.de>
2024-12-05 10:47:57 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-let carrots = 2;
-const dishesCleaned = 0;
+// let carrots = 2;
+// const dishesCleaned = 3;
+// const kidDidHelp = dishesCleaned > 3; // false
+
+// if (kidDidHelp) {
+//   carrots = carrots + 2;
+// } else {
+//   carrots = carrots - 1;
+// }
+// console.log(carrots);
+// let carrots = 2;
+// const dishesCleaned = 1;
+// const kidDidHelpCleaningTheKitchen = dishesCleaned > 3;
+
+//let extraCarrots = kidDidHelpCleaningTheKitchen ? 2 : -1;
+
+// if (kidDidHelpCleaningTheKitchen) {
+//   extraCarrots = 2;
+//   console.log("bla")
+// } else if {
+//   extraCarrots = -1;
+// }
+
+// carrots = carrots + (kidDidHelpCleaningTheKitchen ? 2 : -1);
+// console.log(carrots);
+let carrots;
+
+if (carrots) {
+  console.log("Ja-Zweig");
+} else {
+  console.log("Please give your age");
+}
+// Falsy sind: 0, "", null, undefined, false
